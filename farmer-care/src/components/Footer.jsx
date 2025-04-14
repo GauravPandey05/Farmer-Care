f3778d43984bc56a9ef1d60209c0ad37832648db
Gaurav <gauravp5805@gmail.com>
2025-04-14 18:39:23 +0530

    Update Footer.jsx

--- farmer-care/src/components/Footer.jsx
+++ farmer-care/src/components/Footer.jsx
@@ -19,7 +19,7 @@ function Footer() {
             <h4 className="text-lg font-semibold mb-4">Contact Us</h4>
             <ul className="space-y-2 text-green-200">
               <li>Email: support@farmercare.com</li>
-              <li>Phone: +91 (555) 123-4567</li>
+              <li>Phone: +91 7489923678</li>
               <li>Address: Dwarka Mor</li>
               <li>New Delhi</li>
               <li>Hours: Mon-Fri 9:00 AM - 6:00 PM</li>
